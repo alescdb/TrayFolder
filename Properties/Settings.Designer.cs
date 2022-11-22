e98b776ad7fbd8d1f00c14dd4867931510e7d12e
Alexandre Del Bigio <alexandre-del-bigio@alain-bensoussan.com>
2022-11-22 23:56:06 +0845

    typo

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -11,7 +11,7 @@
 namespace TrayFolder.Properties {
     
     
-    [global::System.Runtime.CompilerServices.CompilerGeneratedAttrcibute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
